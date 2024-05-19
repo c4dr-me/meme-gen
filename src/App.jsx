29937723ceefe7e2e9f2ca94354103793097921c
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-05-20 00:23:45 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from '../components/header'
+import Meme from '../components/meme'
+
+
+export default function App() {
+  return (
+      <div>
+          <Header />
+          <Meme />
+      </div>
+  )
+}
